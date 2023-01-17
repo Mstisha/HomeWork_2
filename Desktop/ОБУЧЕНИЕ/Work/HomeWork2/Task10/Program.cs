f67759916c22c4f977ce2aebcfd307a764db1768
Slava <mistik99@mail.ru>
2023-01-17 20:34:43 +0200

    Задача №10

---
+++ Desktop/ОБУЧЕНИЕ/Work/HomeWork2/Task10/Program.cs
@@ -0,0 +1,15 @@
+﻿int GetNumber()
+{
+    Console.WriteLine("Введите трёхзначное число:");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    return number1;
+}
+
+int DeleteNumber(int number)
+{
+    return (number / 10) % 10;
+}
+
+int number = GetNumber();
+int result = DeleteNumber(number);
+Console.WriteLine($"Было {number}, стало {result}.");

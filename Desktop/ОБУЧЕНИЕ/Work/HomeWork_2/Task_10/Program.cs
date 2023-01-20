3b1854ea33393b0aea025e45316f45c48cf1c0aa
Slava <mistik99@mail.ru>
2023-01-20 16:33:54 +0200

    Task 10

---
+++ Desktop/ОБУЧЕНИЕ/Work/HomeWork_2/Task_10/Program.cs
@@ -0,0 +1,13 @@
+﻿int GetNumber()
+{
+    Console.WriteLine("Введите трёхзначное число:");
+    int numbers = Convert.ToInt32(Console.ReadLine());
+    return numbers;
+}
+
+int DeletNumber(int number)
+{
+    return (number % 100) / 10;
+}
+int number = GetNumber();
+Console.WriteLine(DeletNumber(number));

45458365e3957c7b9db2b606fe024ce36db3173e
Slava <mistik99@mail.ru>
2023-01-20 16:44:04 +0200

    Task 15

---
+++ Desktop/ОБУЧЕНИЕ/Work/HomeWork_2/Task_15/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Введите число:");
+int numbers = Convert.ToInt32(Console.ReadLine());
+
+if(numbers == 6)
+{
+    Console.WriteLine("ДА!");
+}
+else if(numbers == 7)
+{
+    Console.WriteLine("ДА!");
+}
+else
+{
+    Console.WriteLine("НЕТ!");
+}
